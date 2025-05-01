17a52a3ea9cf2b467985ad5703cf84f72c4cdc03
Ivan Ostapenko <Ivan.Ostapenko@norbit.ru>
2025-05-01 13:53:03 +0300

    add cors config

---
+++ src/main/kotlin/ru/project/lab1/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package ru.project.lab1.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+            }
+        }
+    }
+}
